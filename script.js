a16349dee24575603d1e83b418d183dfc023b6ac
kiriki1991 <kirillmerkulov@tut.by>
2018-02-12 22:24:45 +0300

    + Additional styles for 'option' inputs

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-    var $copyLinkBtn = document.querySelector('#cplkbtn'),
-        $copyCodeBtn = document.querySelector('#cpcdbtn');
-    var functionCopyLinkToClipboard = function(event) {
+/* Copy To Clipboard */
+
+var $copyLinkBtn = document.querySelector('#cplkbtn'),
+    $copyCodeBtn = document.querySelector('#cpcdbtn'),
+    functionCopyLinkToClipboard = function (event) {
         var copyLink = document.querySelector('#cplkint');
         copyLink.select();
         document.execCommand('copy');
     }
-    $copyLinkBtn.addEventListener('click', functionCopyLinkToClipboard, false);
-    var functionCopyCodeToClipboard = function(event) {
-        var copyCode = document.querySelector('#cpcdint');
-        copyCode.select();
-        document.execCommand('copy');
-    }
-    $copyCodeBtn.addEventListener('click', functionCopyCodeToClipboard, false);
+$copyLinkBtn.addEventListener('click', functionCopyLinkToClipboard, false);
+
+var functionCopyCodeToClipboard = function (event) {
+    var copyCode = document.querySelector('#cpcdint');
+    copyCode.select();
+    document.execCommand('copy');
+}
+$copyCodeBtn.addEventListener('click', functionCopyCodeToClipboard, false);
+
+/* adding a response option */
+
+function suchMyBalls() {
+    
+     if($inputOption2.getAttribute('checked') == 'checked'){
+            $inputOption2.removeAttribute('checked');
+            document.getElementById('pOption2').style.opacity = '0.4';
+        }else{
+            $inputOption2.setAttribute('checked', 'checked');
+                document.getElementById('pOption2').style.opacity = '1';
+        }
+}
+
+var $pClickedOption = document.querySelector('#pOption2'),
+    $inputOption2 = document.querySelector('#inputOption2');
+$pClickedOption.addEventListener('click', suchMyBalls, false);
