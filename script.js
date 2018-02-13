be27171690477059de9a0b27c8df3eab97e709c6
kiriki1991 <kirillmerkulov@tut.by>
2018-02-13 13:58:29 +0300

    * new style for inputs

--- script.js
+++ script.js
@@ -18,17 +18,32 @@ $copyCodeBtn.addEventListener('click', functionCopyCodeToClipboard, false);
 
 /* adding a response option */
 
-function suchMyBalls() {
-    
-     if($inputOption2.getAttribute('checked') == 'checked'){
-            $inputOption2.removeAttribute('checked');
-            document.getElementById('pOption2').style.opacity = '0.4';
-        }else{
-            $inputOption2.setAttribute('checked', 'checked');
-                document.getElementById('pOption2').style.opacity = '1';
-        }
+function doInputActive() {
+    console.log('doInputActive');
+    document.getElementById('pOptionButt2').removeAttribute('disabled');
+    document.getElementById('pOption2').style.opacity = '1';
+    document.getElementById('inputOption2').setAttribute('placeholder', 'Type your text here');
 }
+
+function remInput() {
+    console.log('remInput');
+    document.getElementById('pOption2').style.opacity = '0.4';
+    document.getElementById('pOptionButt2').setAttribute('disabled', 'disabled');
+    document.getElementById('inputOption2').setAttribute('placeholder', 'Click here to add new one');
+}
+
+function addInputPoll() {
+    console.log('addInputForPoll');
+    var $div = document.createElement('div'),
+        $oRight = document.getElementsByClassName('oRight')[0],
+        $html = chrome.extension.getURL('enrichment.html'),
+        $bRelatedListfirstDiv = document.querySelector('.bRelatedList.first');
+        $div.className = 'bRelatedList.first';
+        $div.innerHTML = `<div`
+}
+
 
 var $pClickedOption = document.querySelector('#pOption2'),
     $inputOption2 = document.querySelector('#inputOption2');
-$pClickedOption.addEventListener('click', suchMyBalls, false);
+$inputOption2.addEventListener('mousedown', doInputActive, false);
+document.getElementById('pOptionButt2').addEventListener('click', remInput, false);
