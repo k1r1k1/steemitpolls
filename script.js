926b502fb7aeca9e5d193d4271b85f54b40ec854
kiriki1991 <kirillmerkulov@tut.by>
2018-03-08 12:20:11 +0300

    * Polling process in progress

--- script.js
+++ script.js
@@ -116,24 +116,30 @@ function completeForm() {
     /* collecting data for sending */
 
     str = module.exports().transform(document.querySelector('.form-control.title').value, '-');
-    str.replace(/[^\w\d]/g, "_");
+    str.replace(/[^\w\d]/g, '_');
+    str = str + '-' + Date.now();
     var title = document.querySelector('.form-control.title').value;
-    /*console.log('permlink : ' + str);
+    console.log('permlink : ' + str);
     console.log('json var : ' + answers); // debug info
-    console.log('title : ' + title);*/
+    console.log('title : ' + title);
     var jsonMetadata = {
-        poll_title: title,
-        poll_answers: answers
+        app: 'golospolls/0.1',
+        canonical: 'https://golospolls.com#' + username + '/' + str,
+        app_account: 'golosapps',
+        data: {
+            poll_title: title,
+            poll_answers: answers
+        }
     };
     console.log(jsonMetadata);
     send_request(str, title, jsonMetadata);
 
     /* visual */
-    
+
     swal({
         type: 'success',
         title: 'Your polling form has been compiled',
-        text: "Don`t forget to share it!",
+        text: 'Don`t forget to share it!',
         showConfirmButton: false,
         timer: 2500
     })
@@ -145,10 +151,15 @@ function completeForm() {
 }
 
 function progress_click() {
-    alert('Вы только что выбрали вариант № ' + this.id);
+    if (wif) {
+        alert('Вы только что выбрали вариант № ' + this.id);
+        sendVote(this.id);
+    } else {
+        auth();
+    }
 }
 
-function send_request(permlink, title, jsonMetadata) {
+function send_request(str, title, jsonMetadata) {
     var parentAuthor = ''; // for post creating, empty field
     var parentPermlink = 'test'; // main tag
     //var username = ''; // post author
@@ -157,7 +168,7 @@ function send_request(permlink, title, jsonMetadata) {
     //var title = 'test'; // post title
     //var jsonMetadata = {}; // jsonMetadata - post metadata (pictures etc.)
     var body = 'At the moment, you are looking at the test page of a simple microservice, which is currently under development. And since it so happened that you look at it, here`s a random cat, good luck to you and all the best.<img src="https://tinygrainofrice.files.wordpress.com/2013/08/kitten-16219-1280x1024.jpg"></img>'; // post text
-    golos.broadcast.comment(wif, parentAuthor, parentPermlink, username, permlink, title, body, jsonMetadata, function (err, result) {
+    golos.broadcast.comment(wif, parentAuthor, parentPermlink, username, str, title, body, jsonMetadata, function (err, result) {
         //console.log(err, result);
         if (!err) {
             console.log('comment', result);
@@ -181,7 +192,7 @@ function getHash() {
     var permlink = hash.substring(slashPos + 1); // '+ 1' removes '/' 
     console.log('username : ' + username);
     console.log('permlink : ' + permlink);
-    
+
     /* The console displays the data required for the post */
 
     golos.api.getContent(username, permlink, function (err, result) {
@@ -195,24 +206,24 @@ function getHash() {
 }
 
 function getPoll() {
-    
+
     /* collecting data */
 
     resultContent.json_metadata = JSON.parse(resultContent.json_metadata); //parse json to js
-    
+
     /* inserting header in poll */
 
     var $div = document.createElement('h5');
     $div.className = 'card-title';
-    $div.innerHTML = resultContent.json_metadata.poll_title;
+    $div.innerHTML = resultContent.json_metadata.data.poll_title;
     document.querySelector('.card-body.text-dark').appendChild($div);
 
-    /* inserting new inputs in poll resultContent.json_metadata.poll_answers[0];*/
+    /* inserting new inputs in poll */
 
-    for (var cnt = 0; resultContent.json_metadata.poll_answers.length - 1 > cnt; cnt++) {
+    for (var cnt = 0; resultContent.json_metadata.data.poll_answers.length > cnt; cnt++) {
         var $div = document.createElement('div');
         $div.className = 'progress-block';
-        $div.innerHTML = `<p class="card-text">` + resultContent.json_metadata.poll_answers[cnt] + `</p>
+        $div.innerHTML = `<p class="card-text">` + resultContent.json_metadata.data.poll_answers[cnt] + `</p>
                     <div class="progress" id="` + cnt + `" style="cursor: pointer;">
                         <div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0</div>
                     </div><br>`;
@@ -231,6 +242,47 @@ function getPoll() {
     document.querySelector('#cplkint').value = 'golospolls.com#' + resultContent.author + '/' + resultContent.permlink;
 }
 
+function sendVote(pollId) {
+    var parentAuthor = resultContent.author;
+    var parentPermlink = resultContent.permlink;
+    var permlink = 're-' + parentAuthor + '-' + parentPermlink + '-' + Date.now(); // re-epexa-test-url-1517333064308
+    var title = ''; // title - для добавления комментария, поле пустое
+    var body = 'I choose option # ' + pollId; // poll
+    var jsonMetadata = {
+        app: 'golospolls/0.1',
+        canonical: 'https://golospolls.com#' + username + '/' + permlink,
+        app_account: 'golosapps',
+        data: {
+            poll_id: pollId
+        }
+    };
+    jsonMetadata = JSON.stringify(jsonMetadata);
+    golos.broadcast.comment(wif, parentAuthor, parentPermlink, username, permlink, title, body, jsonMetadata, function (err, result) {
+        //console.log(err, result);
+        if (!err) {
+            console.log('comment', result);
+        } else console.error(err);
+    });
+}
+
+function getVote() {
+    var pollData = {};
+    golos.api.getContentReplies(resultContent.author, resultContent.permlink, function (err, result) {
+        if (!err) {
+            result.forEach(function (item) {
+                item.json_metadata = JSON.parse(item.json_metadata);
+                
+                if (typeof item.json_metadata.data != 'undefined' && typeof item.json_metadata.data.poll_id != 'undefined') {
+                    console.log(item.json_metadata.data.poll_id);
+                    if ( ! pollData[item.json_metadata.data.poll_id]) pollData[item.json_metadata.data.poll_id] = {count: 0};
+                    pollData[item.json_metadata.data.poll_id].count++;
+                }
+            });
+            console.log(pollData); //число повторений для каждого элемента массива
+        } else console.error(err);
+    });
+}
+
 /* buttons events */
 
 document.getElementById('pOptionButt1').addEventListener('click', doInputInactive, false);
@@ -238,7 +290,7 @@ document.getElementById('pOptionButt2').addEventListener('click', doInputInactiv
 document.getElementById('complete').addEventListener('click', function () {
     swal({
         title: 'Are you sure?',
-        text: "You won't be able to revert this!",
+        text: 'You won`t be able to revert this!',
         type: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
