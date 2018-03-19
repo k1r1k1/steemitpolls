0e42a8adfc79a81a05d5cefba4e434aa65fb08b7
kiriki1991 <kirillmerkulov@tut.by>
2018-03-20 01:38:34 +0300

    * language lib initializing

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 /* GolosPolls main script file */
 
 //-----------------------------/
-
-// switching to testnet 
+initLang('en'); // lang init = en
+// switching to testnet
 golos.config.set('websocket', 'wss://ws.testnet3.golos.io');
 golos.config.set('address_prefix', 'GLS');
 golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
