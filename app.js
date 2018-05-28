0fcc160a125a73baa33c5baf709d711ae544d499
kiriki1991 <kirillmerkulov@tut.by>
2018-05-28 20:12:59 +0300

    * translation_string_add&auth_patch

--- app.js
+++ app.js
@@ -5,6 +5,10 @@
 // switching to testnet
 /*golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
 golos.config.set('websocket', 'wss://ws.testnet.golos.io');*/
+
+var wif = localStorage.wif, // auth.js patch
+	username = localStorage.username;
+
 var inputsC = 0; // inputs counter
 initLang('en'); // lang init = en
 if (hash != '') {
@@ -294,8 +298,8 @@ function getMyPolls(callback) {
 			document.querySelector('.lding').style.display = 'none';
 		}
 		if (!err) {
+			document.querySelector('#complete-form .card-header').innerHTML = 'Make your choice';
 			result.forEach(function (item) {
-				document.querySelector('#complete-form .card-header').innerHTML = 'Make your choice';
 				golos.api.getContentReplies(item.author, item.permlink, function (err, result) {
 					if (!err) {
 						pollData = {};
