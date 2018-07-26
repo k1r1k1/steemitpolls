da0d7e5c0074d5058375b37b42cbe7840511c9f6
kiriki1991 <kirillmerkulov@tut.by>
2018-07-26 14:37:16 +0300

    js translation fix

--- app.js
+++ app.js
@@ -258,6 +258,7 @@ function getMyPolls(callback) {
 	clearUpdTimer();
 	document.querySelector('.lding').style.display = 'block';
 	document.querySelector('.rem-vote').style.display = 'none';
+	document.querySelector('.edit-poll').style.display = 'none';
 	location.hash = '';
 	document.querySelector('.card-header-right p').innerHTML = '';
 	document.getElementById('complete-form').style.display = 'block';
@@ -501,13 +502,13 @@ document.querySelector('.edit-poll').addEventListener('click', () => {
 	auth(function () {
 		swal({
 			html: `<div class="form-group-swal">
-							<label>`+ document.querySelector('#complete-form .card-header').innerHTML = document.querySelectorAll('.translate-phrases li')[23].innerHTML +`</label>
+							<label>` + document.querySelectorAll('.translate-phrases li')[23].innerHTML + `</label>
 							<input type="text" class="form-control title edit" value="` + resultContent.json_metadata.data.poll_title + `" placeholder="Type your text here">
-							<label for="exampleFormControlTextarea1">`+ document.querySelector('#complete-form .card-header').innerHTML = document.querySelectorAll('.translate-phrases li')[24].innerHTML +`</label>
+							<label for="exampleFormControlTextarea1">` + document.querySelectorAll('.translate-phrases li')[24].innerHTML + `</label>
 							<textarea class="form-control" id="pollDescriptionInput" rows="3" maxlength="300">` + resultContent.json_metadata.data.poll_description + `</textarea>
 							<br><img class="uplded-img-true" id="load-imag" src="` + resultContent.json_metadata.data.title_image + `" width="34" height="34" onClick="ipfsImgLoad(this)"><img class="uplded-img" id="load-imag" src="` + resultContent.json_metadata.data.title_image + `" width="34" height="34" style="margin: 0 -39px;" onClick="ipfsImgLoad(this)">
 						<div id="EditPollForm">
-							<label>`+ document.querySelector('#complete-form .card-header').innerHTML = document.querySelectorAll('.translate-phrases li')[25].innerHTML +`</label>
+							<label>` + document.querySelectorAll('.translate-phrases li')[25].innerHTML + `</label>
 						</div>
 						</div><div class="varDiv">` + pollHTML + `</div>`,
 			showCloseButton: true,
