530e4da9c8aa1e15330f586c2f03fbaf508f1d0c
kiriki1991 <kirillmerkulov@tut.by>
2018-08-01 15:34:15 +0300

    dropzone integration test1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 // switching to testnet
 golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
 golos.config.set('websocket', 'wss://ws.testnet.golos.io');
+Dropzone.autoDiscover = false;
 
 var inputsC = 0,
 	newPostTimout; // inputs counter
@@ -40,7 +41,7 @@ document.onreadystatechange = function () { // loading animation switch-off
 		document.querySelector('#inputOption2').value = 'Second option';
 		document.querySelector('.footer').scrollIntoView();
 		// end autofill
-// variable in valid format moment.js
+		// variable in valid format moment.js
 		switch (localStorage.lang) {
 			case 'ua':
 				currentLang = 'uk';
@@ -637,3 +638,93 @@ document.getElementById('integration').addEventListener('click', () => {
 		showCloseButton: 'true'
 	});
 }, false);
+
+	/* dropzone script */
+
+// Get the template HTML and remove it from the doument
+var previewNode = document.querySelector("#template");
+previewNode.id = "";
+var previewTemplate = previewNode.parentNode.innerHTML;
+previewNode.parentNode.removeChild(previewNode);
+
+var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+	url: "http://www.torrentplease.com/dropzone.php", // Set the url
+	thumbnailWidth: 80,
+	thumbnailHeight: 80,
+	parallelUploads: 20,
+	previewTemplate: previewTemplate,
+	autoQueue: false, // Make sure the files aren't queued until manually added
+	previewsContainer: "#previews", // Define the container to display the previews
+	clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+});
+
+/*myDropzone.on("addedfile", function(file) {
+  // Hookup the start button
+  file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file); };
+});*/
+
+// Update the total progress bar
+myDropzone.on("totaluploadprogress", function (progress) {
+	document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
+});
+
+myDropzone.on("sending", function (file) {
+	// Show the total progress bar when upload starts
+	document.querySelector("#total-progress").style.opacity = "1";
+	// And disable the start button
+	//  file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+});
+
+// Hide the total progress bar when nothing's uploading anymore
+myDropzone.on("queuecomplete", function (progress) {
+	document.querySelector("#total-progress").style.opacity = "0";
+});
+
+// Setup the buttons for all transfers
+// The "add files" button doesn't need to be setup because the config
+// `clickable` has already been specified.
+document.querySelector("#actions .start").onclick = function () {
+	myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+};
+/*  document.querySelector("#actions .cancel").onclick = function() {
+    myDropzone.removeAllFiles(true);
+  };*/
+
+// Now fake the file upload, since GitHub does not handle file uploads
+// and returns a 404
+
+var minSteps = 6,
+	maxSteps = 60,
+	timeBetweenSteps = 100,
+	bytesPerStep = 100000;
+
+myDropzone.uploadFiles = function (files) {
+	var self = this;
+
+	for (var i = 0; i < files.length; i++) {
+
+		var file = files[i];
+		totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+
+		for (var step = 0; step < totalSteps; step++) {
+			var duration = timeBetweenSteps * (step + 1);
+			setTimeout(function (file, totalSteps, step) {
+				return function () {
+					file.upload = {
+						progress: 100 * (step + 1) / totalSteps,
+						total: file.size,
+						bytesSent: (step + 1) * file.size / totalSteps
+					};
+
+					self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
+					if (file.upload.progress == 100) {
+						file.status = Dropzone.SUCCESS;
+						self.emit("success", file, 'success', null);
+						self.emit("complete", file);
+						self.processQueue();
+					}
+				};
+			}(file, totalSteps, step), duration);
+		}
+	}
+}
