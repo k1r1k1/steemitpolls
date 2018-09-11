19b61554f5f36a30e1c699f77ff4fa9e90d3cae6
kiriki1991 <kirillmerkulov@tut.by>
2018-09-11 23:55:38 +0300

    errors return fix

--- golosErrs.js
+++ golosErrs.js
@@ -1,18 +1,18 @@
 /* This library makes standard logs of "Steemit"/"Golos" errors more user-friendly */
 
 function humaNize(errors) {
-	alert(errors);
+	console.log('caught:',errors);
 	if (errors.cause) {
 		errors = JSON.stringify(errors.cause.payload.error.data.stack[0].data.error.stack[0].format);
-		alert(errors);
-		console.log(errors);
+		console.log('cleared:', errors);
 		var eLang = 'errs/' + detectLang() + '.json',
 			request = new XMLHttpRequest();
 		request.open('GET', eLang, false);
 		request.send(null);
 		var my_JSON_object = JSON.parse(request.responseText);
-		console.log ('found:', my_JSON_object[errors]);
+		console.log('found:', my_JSON_object[errors]);
+		return my_JSON_object[errors]
 	} else {
-
+		return errors
 	}
 }
