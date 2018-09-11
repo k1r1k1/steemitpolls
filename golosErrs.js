94a94e42cd0c97126c3fa87ecf25a5f644c5f53b
kiriki1991 <kirillmerkulov@tut.by>
2018-09-12 00:09:06 +0300

    more exceptions for errors

--- golosErrs.js
+++ golosErrs.js
@@ -2,14 +2,17 @@
 
 function humaNize(errors) {
 	console.log('caught:',errors);
-	if (errors.cause) {
-		errors = JSON.stringify(errors.cause.payload.error.data.stack[0].data.error.stack[0].format);
-		console.log('cleared:', errors);
-		var eLang = 'errs/' + detectLang() + '.json',
+	var eLang = 'errs/' + detectLang() + '.json',
 			request = new XMLHttpRequest();
 		request.open('GET', eLang, false);
 		request.send(null);
 		var my_JSON_object = JSON.parse(request.responseText);
+	if (errors.cause) {
+		errors = JSON.stringify(errors.cause.payload.error.data.stack[0].data.error.stack[0].format);
+		console.log('cleared:', errors);
+		console.log('found:', my_JSON_object[errors]);
+		return my_JSON_object[errors]
+	} else if (my_JSON_object[errors] != undefined) {
 		console.log('found:', my_JSON_object[errors]);
 		return my_JSON_object[errors]
 	} else {
